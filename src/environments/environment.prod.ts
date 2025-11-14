35a4192a7b0102f761623ae22d398eec959f6df2
Ajagbe Uthman Olawale <uthman4u2nv@yahoo.com>
2025-11-14 08:26:07 +0100

    First Build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import packageInfo from '../../package.json';
 
 export const environment = {
   appVersion: packageInfo.version,
-  production: true
+  production: true,
+  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpmZG54cGFqanBnaW55d3R6bHd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI3ODEwOTYsImV4cCI6MjA3ODM1NzA5Nn0.Yeux1nQ7cjbral86PMRDkL9-eEi5B4Zv4JtAnw2oVc4'
 };
